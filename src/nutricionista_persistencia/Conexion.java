88392f29f956141213d6e9d49afd68de5a8ed0a2
MMercedes2023 <141679757+MMercedes2023@users.noreply.github.com>
2024-10-22 19:58:10 -0300

    clase conexion

---
+++ src/nutricionista_persistencia/Conexion.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nutricionista_persistencia;
+
+/**
+ *
+ * @author maria
+ */
+public class Conexion {
+    
+}
