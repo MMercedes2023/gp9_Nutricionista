b86cc18db09c68be454a950b1509843b8817f2ee
Florencia Badiali <badialiflor@gmail.com>
2024-10-22 19:33:24 -0300

    Subiendo script+vistaDiseñador

---
+++ basededatos/GP9_nutricionista_base.sql
@@ -0,0 +1,153 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: localhost:3307
+-- Tiempo de generación: 23-10-2024 a las 00:24:39
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `nutricionista`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `comida`
+--
+
+CREATE TABLE `comida` (
+  `codComida` int(11) NOT NULL,
+  `nombre` varchar(60) NOT NULL,
+  `tipoComida` varchar(20) NOT NULL,
+  `caloriasPor100g` int(11) NOT NULL,
+  `detalle` varchar(100) NOT NULL,
+  `baja` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `dieta`
+--
+
+CREATE TABLE `dieta` (
+  `codDieta` int(11) NOT NULL,
+  `nombreD` text NOT NULL,
+  `fechaInicio` date NOT NULL,
+  `fechaFin` date NOT NULL,
+  `pesoFinal` float NOT NULL,
+  `estado` tinyint(1) NOT NULL,
+  `totalCalorias` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `menudiario`
+--
+
+CREATE TABLE `menudiario` (
+  `codMenu` int(11) NOT NULL,
+  `diaNro` int(11) NOT NULL,
+  `estado` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `paciente`
+--
+
+CREATE TABLE `paciente` (
+  `nroPaciente` int(11) NOT NULL,
+  `nombre` text NOT NULL,
+  `edad` int(11) NOT NULL,
+  `altura` float NOT NULL,
+  `pesoActual` float NOT NULL,
+  `pesoBuscado` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `renglondemenu`
+--
+
+CREATE TABLE `renglondemenu` (
+  `nroRenglon` int(11) NOT NULL,
+  `cantidadGrs` double NOT NULL,
+  `subtotalCalorias` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `comida`
+--
+ALTER TABLE `comida`
+  ADD PRIMARY KEY (`codComida`);
+
+--
+-- Indices de la tabla `dieta`
+--
+ALTER TABLE `dieta`
+  ADD PRIMARY KEY (`codDieta`);
+
+--
+-- Indices de la tabla `menudiario`
+--
+ALTER TABLE `menudiario`
+  ADD PRIMARY KEY (`codMenu`);
+
+--
+-- Indices de la tabla `paciente`
+--
+ALTER TABLE `paciente`
+  ADD PRIMARY KEY (`nroPaciente`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `comida`
+--
+ALTER TABLE `comida`
+  MODIFY `codComida` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `dieta`
+--
+ALTER TABLE `dieta`
+  MODIFY `codDieta` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `menudiario`
+--
+ALTER TABLE `menudiario`
+  MODIFY `codMenu` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `paciente`
+--
+ALTER TABLE `paciente`
+  MODIFY `nroPaciente` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
